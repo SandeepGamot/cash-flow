97d5d1fa6c0b5cb327f23d6b5a36f4d5a9ae30e8
Sandeep Gamot <sandeep.gamot@betterworks.com>
2024-12-11 21:35:47 +0530

    refactor: move services into a single networks and assign static ips to containers

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-CLIENT_NETWORK_NAME="cashflow-client"
-CLIENT_SUBNET="10.7.0.0/28" # max 2^4 hosts less default gateway, update CIDR as per your needs
+NETWORK_NAME="cashflow"
+SUBNET="10.7.0.0/28" # max 2^4 hosts less default gateway, update CIDR as per your needs
 
-docker network inspect $CLIENT_NETWORK_NAME > /dev/null 2>&1 || docker network create --subnet=$CLIENT_SUBNET $CLIENT_NETWORK_NAME
+docker network inspect $NETWORK_NAME > /dev/null 2>&1 || docker network create --subnet=$SUBNET $NETWORK_NAME
 
-SERVER_NETWORK_NAME="cashflow-server"
-
-docker network inspect $SERVER_NETWORK_NAME > /dev/null 2>&1 || docker network create $SERVER_NETWORK_NAME
-
-docker compose up
+docker compose up --build
