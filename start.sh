fd4282ccf04d7f102e8e7eca12d0e9afeca5184a
Sandeep Gamot <sandeep.gamot@betterworks.com>
2024-12-21 08:44:59 +0530

    add nginx for internal request proxying

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ SUBNET="10.7.0.0/28" # max 2^4 hosts less default gateway, update CIDR as per yo
 
 docker network inspect $NETWORK_NAME > /dev/null 2>&1 || docker network create --subnet=$SUBNET $NETWORK_NAME
 
-docker compose up --build
+docker compose up --build --force-recreate
