6b212825868d482dc884f3bc091283cdd02b9a6a
Sandeep Gamot <sandeep.gamot@betterworks.com>
2024-12-21 12:34:15 +0530

    refactor: simplify docker networking and use internal-dns instead of static ips

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-NETWORK_NAME="cashflow"
-SUBNET="10.7.0.0/28" # max 2^4 hosts less default gateway, update CIDR as per your needs
-
-docker network inspect $NETWORK_NAME > /dev/null 2>&1 || docker network create --subnet=$SUBNET $NETWORK_NAME
-
+source api/.env
 docker compose up --build --force-recreate
